752b04816ec9a6fd2497eb630fac6a42ee4f48c3
Lukas Bouhlel <lukas.bouhlel@epitech.eu>
2024-04-17 14:02:00 +0200

    userValidation

--- app/models/account.js
+++ app/models/account.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const accountSchema = new mongoose.Schema({
-    bankName : { type: String },
-    customName: { type: String },
+    bankName: { type: String, required: true },
+    customName: { type: String, maxlength: 50 },
     lastUpdated: { type: Date },
-    user: { type: String},
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 const Account = mongoose.model('Account', accountSchema);
--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String },
-  password: { type: String },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: props => `${props.value} n'est pas une email valide !`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{9,}$/.test(v);
+            },
+            message: `Le mot de passe invalide ! Il doit contenir au moins 9 caractères dont au moins une lettre majuscule, un symbole et un chiffre.`
+        }
+    }
 });
 
 const User = mongoose.model("User", userSchema);
